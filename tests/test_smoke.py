70a0a65b9841d03efebfd34d75fb8ab543980498
Kyle Barron <kyle@developmentseed.org>
2026-06-16 16:40:20 -0400

    remove empty test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,7 +4,3 @@ import zarrsita
 def test_version_is_nonempty_string():
     assert isinstance(zarrsita.__version__, str)
     assert zarrsita.__version__
-
-
-def test_hello():
-    assert zarrsita.hello() == "Hello from zarrsita!"
