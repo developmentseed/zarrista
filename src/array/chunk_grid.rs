e99b0715bc4b8c20a853acfdd3a5adb2f9e6eeda
Kyle Barron <kyle@developmentseed.org>
2026-06-23 14:39:46 -0400

    more chunk grid options

--- src/array/chunk_grid.rs
+++ src/array/chunk_grid.rs
@@ -1,7 +1,11 @@
 use std::sync::Arc;
 
 use pyo3::prelude::*;
-use zarrs::array::chunk_grid::{RectilinearChunkGrid, RegularBoundedChunkGrid};
+use pyo3::types::PyInt;
+use zarrs::array::chunk_grid::{
+    ChunkEdgeLengths, RectilinearChunkGrid, RegularBoundedChunkGrid, RegularChunkGrid,
+    RunLengthElement,
+};
 use zarrs::array::ChunkGrid;
 
 use crate::array::{PyArrayShape, PyChunkShape};
@@ -24,6 +28,23 @@ impl PyChunkGrid {
 
 #[pymethods]
 impl PyChunkGrid {
+    #[staticmethod]
+    fn rectilinear(
+        array_shape: PyArrayShape,
+        chunk_shapes: Vec<PyChunkEdgeLengths>,
+    ) -> ZarristaResult<Self> {
+        let chunk_shapes = chunk_shapes.into_iter().map(|c| c.0).collect::<Vec<_>>();
+        let chunk_grid = RectilinearChunkGrid::new(array_shape.into_inner(), &chunk_shapes)?;
+        Ok(Self(Arc::new(chunk_grid).into()))
+    }
+
+    #[staticmethod]
+    fn regular(array_shape: PyArrayShape, chunk_shape: PyChunkShape) -> ZarristaResult<Self> {
+        let chunk_grid = RegularChunkGrid::new(array_shape.into_inner(), chunk_shape.into_inner())?;
+        Ok(Self(Arc::new(chunk_grid).into()))
+    }
+
+    /// This chunk grid is experimental and may be incompatible with other Zarr V3 implementations.
     #[staticmethod]
     fn regular_bounded(
         array_shape: PyArrayShape,
@@ -69,5 +90,36 @@ impl From<ChunkGrid> for PyChunkGrid {
 impl From<PyChunkGrid> for ChunkGrid {
     fn from(py_chunk_grid: PyChunkGrid) -> Self {
         py_chunk_grid.0
+    }
+}
+
+pub struct PyChunkEdgeLengths(ChunkEdgeLengths);
+
+impl FromPyObject<'_, '_> for PyChunkEdgeLengths {
+    type Error = PyErr;
+
+    fn extract(obj: Borrowed<'_, '_, PyAny>) -> Result<Self, Self::Error> {
+        if obj.is_instance_of::<PyInt>() {
+            Ok(Self(ChunkEdgeLengths::Scalar(obj.extract()?)))
+        } else {
+            let elements = obj.extract::<Vec<PyRunLengthElement>>()?;
+            Ok(Self(ChunkEdgeLengths::Varying(
+                elements.into_iter().map(|e| e.0).collect(),
+            )))
+        }
+    }
+}
+
+pub struct PyRunLengthElement(RunLengthElement);
+
+impl FromPyObject<'_, '_> for PyRunLengthElement {
+    type Error = PyErr;
+
+    fn extract(obj: Borrowed<'_, '_, PyAny>) -> Result<Self, Self::Error> {
+        if obj.is_instance_of::<PyInt>() {
+            Ok(Self(RunLengthElement::Single(obj.extract()?)))
+        } else {
+            Ok(Self(RunLengthElement::Repeated(obj.extract()?)))
+        }
     }
 }
