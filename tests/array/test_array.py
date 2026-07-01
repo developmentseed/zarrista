cce3d796bb666296fec0ad1be8c0a692a5edd45c
Davis Vann Bennett <davis.v.bennett@gmail.com>
2026-07-01 17:07:33 +0200

    chore: lint

--- tests/array/test_array.py
+++ tests/array/test_array.py
@@ -1,6 +1,7 @@
 from zarr_metadata import ArrayMetadataV3
 import zarrista
 
+
 def test_array_from_metadata():
     """
     Test that Array.from_metadata works
@@ -12,7 +13,7 @@ def test_array_from_metadata():
         "attributes": {},
         "chunk_grid": {"name": "regular", "configuration": {"chunk_shape": (2, 2)}},
         "data_type": "int16",
-        "chunk_key_encoding": {"name": "default", "configuration": {"separator": "/" }},
+        "chunk_key_encoding": {"name": "default", "configuration": {"separator": "/"}},
         "fill_value": 0,
         "node_type": "array",
         "shape": (4, 4),
