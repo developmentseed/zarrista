acd384998f663403097e1c6b1cf773ad8cbfa92f
Davis Bennett <davis.v.bennett.git@gmail.com>
2026-07-01 22:23:03 +0200

    fix: Patch upstream monostate-serde bug (#72)
    * test: add failing test
    
    * use serde:Value as intermediate when moving from python representation of JSON metadata to ensure that ints are consistently stored as u64, which zarrs_metadata expects
    
    * chore: lint
    
    * chore: cargo fmt
    
    * chore: ruff
    
    * chore: ruff
    
    * use patched monostate
    
    ---------
    
    Co-authored-by: Kyle Barron <kyle@developmentseed.org>

---
+++ tests/array/test_array.py
@@ -0,0 +1,27 @@
+from typing import TYPE_CHECKING
+
+import zarrista
+
+if TYPE_CHECKING:
+    from zarr_metadata import ArrayMetadataV3
+
+
+def test_array_from_metadata():
+    """
+    Test that Array.from_metadata works
+    """
+    store = zarrista.MemoryStore()
+
+    meta: ArrayMetadataV3 = {
+        "zarr_format": 3,
+        "attributes": {},
+        "chunk_grid": {"name": "regular", "configuration": {"chunk_shape": (2, 2)}},
+        "data_type": "int16",
+        "chunk_key_encoding": {"name": "default", "configuration": {"separator": "/"}},
+        "fill_value": 0,
+        "node_type": "array",
+        "shape": (4, 4),
+        "codecs": ({"name": "bytes"},),
+    }
+
+    zarrista.Array.from_metadata(meta, store)
