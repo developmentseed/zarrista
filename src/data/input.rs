781b8da768db8822ea7d7181ee9a81d20fe85c07
Kyle Barron <kyle@developmentseed.org>
2026-08-03 16:47:54 -0400

    scaffold extractor

--- src/data/input.rs
+++ src/data/input.rs
@@ -62,8 +62,22 @@ impl FromPyObject<'_, '_> for PyDataInput {
     type Error = PyErr;
 
     fn extract(obj: Borrowed<'_, '_, PyAny>) -> Result<Self, Self::Error> {
-        todo!()
+        // 1. Check for literal PyArrayBytes pyclass
+        if let Ok(array_bytes) = obj.cast::<PyArrayBytes>() {
+            return Ok(Self::ArrayBytes(array_bytes.get().clone()));
+        }
+
+        // 2. DLPack extraction
+        if obj.hasattr(intern!(obj.py(), "__dlpack__"))? {
+            return import_dlpack(obj);
+        }
+
+        // Anything else that exposes bytes. There is no type information here,
+        // so the caller has opted out of the data type and shape checks.
+        Ok(Self::Bytes(obj.extract::<PyBytes>()?))
     }
+}
+
 }
 
 /// The Zarr v3 name of `data_type`, for use in a user-facing message.
